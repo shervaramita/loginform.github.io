43f7dea0f5567ebe2dd309b0d14d799ffa184e6c
shervaramita <99843388+shervaramita@users.noreply.github.com>
2022-02-23 04:57:39 +0530

    my node project

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const homeRouter = require('./route/homeRouter')
+const port  = process.env.port || 8080;
+
+const app  = express();
+
+// db con
+
+mongoose.connect('mongodb://localhost:27017/studentsdata',{useNewUrlParser:true})
+const db = mongoose.connection;
+
+db.on("error",()=>{console.log("error in conection");})
+db.once('open',()=>{console.log("mongodb connected");})
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/', homeRouter)
+
+app.listen(port,()=>{
+    console.log("server is connected")
+}) 
